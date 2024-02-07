4cec5d4903a2f370889b678707655fd7721d795e
Teis Loyche <teisloyche@hotmail.com>
2024-02-07 17:05:51 +0100

    added Mongo auditing annotation for created date

--- LibraryServerJava/src/main/java/library/LibraryServerJava/LibraryServerJavaApplication.java
+++ LibraryServerJava/src/main/java/library/LibraryServerJava/LibraryServerJavaApplication.java
@@ -2,12 +2,13 @@ package library.LibraryServerJava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class LibraryServerJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryServerJavaApplication.class, args);
 	}
-
 }
